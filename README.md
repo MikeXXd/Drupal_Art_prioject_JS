7a0678c4c5beacd96216dcc88f82eb798b9f5577
MikeXXd <122735478+MikeXXd@users.noreply.github.com>
2023-10-03 22:17:55 +0200

    Final Tuning and added ZoomInfo

---
+++ README.md
@@ -0,0 +1,17 @@
+Zadání:
+
+1.Nainstaluj/stáhni si wavesurfer.js (knihovnu na převod zvuku do audio stopy) - https://www.npmjs.com/package/wavesurfer.js
+
+2.Vytvoř (podle dokumentace) pod zvukovou stopou 3 funkční elementy:
+a - vícenásobný zoom in (5x)
+b - následný zoom out (5x)
+c - aktuální čas (takže když před samotným spuštěním přehrávače klikneš na půlku, ukáže ti to tento čas)
+
+3.Zařiď, aby čas písně po kliknutí na zvukovou stopu ukazoval správnou hodnotu i v zoom in.
+
+
+
+Coded by Michal Viliš, 
+mike3d @ seznam.cz,
+Frýdland nad Ostravicí, 
+2023
--- script.js
+++ script.js
@@ -1,11 +1,10 @@
 import WaveSurfer from "https://unpkg.com/wavesurfer.js@7/dist/wavesurfer.esm.js";
 
-// preset-----------------------------------start--
-                            
-const  MIN_PX_PER_SEC = 5; // ideal MIN_PX_PER_SEC for screen width: 1920px is 5
+// presets----------------------------------
+const MIN_PX_PER_SEC = 5; // ideal MIN_PX_PER_SEC for screen width: 1920px is 5, better to deal with it dynamically, but it's not the point of this project. See function init() at the end of this file.
+let INITIAL_ZOOM = 0; // 0 = no zoom
 
-// preset-----------------------------------end--
-
+// DOM elements------------------------------
 const zoomInBtn = document.getElementById("zoomInBtn");
 const zoomOutBtn = document.getElementById("zoomOutBtn");
 const actualTime = document.getElementById("currentTime");
@@ -13,8 +12,9 @@ const playBtn = document.getElementById("playBtn");
 const stopBtn = document.getElementById("stopBtn");
 const muteBtn = document.getElementById("muteBtn");
 const playing = document.getElementById("music");
+const zoomInfo = document.getElementById("zoomInfo");
 
-
+// Wavesurfer instance-----------------------
 const wavesurfer = WaveSurfer.create({
   container: "#waveform",
   waveColor: "#A8DBA8",
@@ -28,6 +28,25 @@ const wavesurfer = WaveSurfer.create({
 
 // console.log(wavesurfer.options);
 
+// Supporting functions----------------------------
+function formatTime(timeInSeconds) {
+  const minutes = Math.floor(timeInSeconds / 60);
+  const seconds = Math.floor(timeInSeconds % 60);
+  const milliseconds = Math.floor(
+    (timeInSeconds - Math.floor(timeInSeconds)) * 100
+  );
+  return `${minutes < 10 ? "0" : ""}${minutes}:${
+    seconds < 10 ? "0" : ""
+  }${seconds}.${milliseconds < 10 ? "0" : ""}${milliseconds}`;
+}
+
+function zoomStatusUpdate(number) {
+  let zoomStatus = INITIAL_ZOOM + number;
+  INITIAL_ZOOM = zoomStatus;
+  zoomInfo.textContent = `${zoomStatus} x`;
+}
+
+// Event listeners----------------------------------
 playBtn.onclick = () => {
   wavesurfer.playPause();
   if (playBtn.src.includes("play.png")) {
@@ -35,12 +54,9 @@ playBtn.onclick = () => {
     playing.classList.add("playing");
   } else {
     playBtn.src = "./media/play.png";
-    playing.classList.remove("playing")
-
+    playing.classList.remove("playing");
   }
 };
-
-
 
 stopBtn.onclick = () => {
   wavesurfer.stop();
@@ -60,31 +76,20 @@ zoomInBtn.onclick = () => {
     return;
   }
   value *= 2;
+  zoomStatusUpdate(1);
   wavesurfer.zoom(value);
-  console.log("current minPxPerSec", value);
 };
 
 zoomOutBtn.onclick = () => {
   let value = wavesurfer.options.minPxPerSec;
   if (value > MIN_PX_PER_SEC) {
     value /= 2;
+    zoomStatusUpdate(-1);
   } else {
     return;
   }
   wavesurfer.zoom(value);
-  console.log("current minPxPerSec", value);
 };
-
-function formatTime(timeInSeconds) {
-  const minutes = Math.floor(timeInSeconds / 60);
-  const seconds = Math.floor(timeInSeconds % 60);
-  const milliseconds = Math.floor(
-    (timeInSeconds - Math.floor(timeInSeconds)) * 100
-  );
-  return `${minutes < 10 ? "0" : ""}${minutes}:${
-    seconds < 10 ? "0" : ""
-  }${seconds}.${milliseconds < 10 ? "0" : ""}${milliseconds}`;
-}
 
 wavesurfer.on(
   "timeupdate",
@@ -96,13 +101,7 @@ wavesurfer.on("finish", () => {
   wavesurfer.stop();
 });
 
-
-
-
-
-
-
-// NOT IN USE / JUST CONSOLE INFO.----------------------------------------start
+// Not finished Function - NOT IN PROPER USE / JUST INFO IN CONSOLE NOW ----- The challenge is to find way to provide initial value(currently served by "const MIN_PX_PER_SEC") for creation of wavesurfer instance, and then change it by this function, functionalities ZoomIn and ZoomOut depends on the "const MIN_PX_PER_SEC"-------------------------------------------------------
 async function init() {
   let div = document.getElementById("waveform");
   let pixels = 0;
@@ -118,4 +117,3 @@ async function init() {
 }
 
 init();
-// NOT IN USE / JUST CONSOLE INFO.----------------------------------------end
